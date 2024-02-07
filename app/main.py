7af3613cc82964bcf9acf209879f59fea71a36fb
pharmund <sashkinapochta@inbox.ru>
2024-02-07 16:39:49 +0300

    9

--- app/main.py
+++ app/main.py
@@ -106,6 +106,22 @@ async def get_headers(request: Request) -> dict:
         "Accept-Language": request.headers["accept-language"]
     }
 
+@app.get("/headers3")
+async def get_headers(request: Request) -> dict:
+    check_headers(request.headers)
+    return {
+        "User-Agent" : request.headers["user-agent"],
+        "Accept-Language": request.headers["accept-language"]
+    }
+
+@app.get("/headers4")
+async def get_headers(request: Request) -> dict:
+    check_headers(request.headers)
+    return {
+        "User-Agent" : request.headers["user-agent"],
+        "Accept-Language": request.headers["accept-language"]
+    }
+
 
 
 
